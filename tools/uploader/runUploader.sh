17dd7e5a4b2d6358a41adbf2686872d38f70343f
alireza <ali64iust@gmail.com>
2022-01-23 06:56:35 +0000

    Initial commit2

--- tools/uploader/runUploader.sh
+++ tools/uploader/runUploader.sh
@@ -1,2 +1,2 @@
 mkdir /upload
-docker run --rm -it -v "/upload:/var/www/html/uploads/" -p 32773:80 adrianlzt/docker-nginx-php-upload
+docker run --rm -it -d -v "/upload:/var/www/html/uploads/" -p 32773:80 adrianlzt/docker-nginx-php-upload
